40e3990783ad1babb6266f66700bdd091633f6fd
pedro.mendes <pedro.mendes@g.globo>
2024-05-15 09:17:22 -0300

    minor: injecting a dummy access token to client requests

--- client/hello.go
+++ client/hello.go
@@ -6,14 +6,20 @@ import (
 	"log"
 
 	pb "github.com/pedrolopeme/grpc-auth-middleware/proto"
+	"google.golang.org/grpc/metadata"
 )
 
-func doHello(client pb.HelloHandlerClient, name string) {
+func doHello(client pb.HelloHandlerClient, name string, accessToken string) {
 	fmt.Printf("doHello was invoked for %v\n", name)
 
-	res, err := client.SayHello(context.Background(), &pb.Hello{
-		Name: name,
-	})
+	// Add the access token to the request metadata.
+	ctx := metadata.NewOutgoingContext(context.Background(), metadata.Pairs("authorization", accessToken))
+
+	// building the message
+	message := &pb.Hello{Name: name}
+
+	// Sending message
+	res, err := client.SayHello(ctx, message)
 	if err != nil {
 		log.Fatalf("Could not greet: %v", err)
 	}
